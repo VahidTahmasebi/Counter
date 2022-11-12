c2941170b5ec670fc96618fa2e8bc20870cab17f
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-11-12 21:38:49 +0330

    Make counter in js file

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const btn = document.querySelector(".btn");
-const counter = document.querySelector(".container h1");
+const btn = document.querySelectorAll(".btn");
+const counter = document.querySelector(".number");
+
+let newNumber = 0;
+
+// Get the requested class and apply the command
+btn.forEach((e) => {
+  e.addEventListener("click", () => {
+    const classResolve = e.classList;
+    if (classResolve.contains("increase")) newNumber++;
+    else if (classResolve.contains("decrease")) newNumber--;
+    else newNumber = 0;
+    counter.textContent = newNumber;
+  });
+});
